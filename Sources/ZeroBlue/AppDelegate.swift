4786916f5505f7a152bc4a3dfd926b90812e1f18
Kevin Liao <kevin.liao.cheetah@gmail.com>
2026-07-24 09:00:39 -0700

    Fix AppDelegate build errors
    applicationDidFinishLaunching typo meant it never fired; broken
    parens around the enabled-flag ternary; wrong UserDefaults key
    for intensity; #objc -> @objc; guard bound the NSStatusItem
    instead of its .button; GammaController case typo.

--- Sources/ZeroBlue/AppDelegate.swift
+++ Sources/ZeroBlue/AppDelegate.swift
@@ -5,3 +5,45 @@
 //  Created by Kevin Liao on 7/24/26.
 //
 
+import AppKit
+import SwiftUI
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "sun.max.fill", accessibilityDescription: "ZeroBlue")
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+        
+        popover = NSPopover()
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        
+        let intensity = UserDefaults.standard.double(forKey: "zb.intensity")
+        let enabled = UserDefaults.standard.object(forKey: "zb.enabled") == nil
+            ? true
+            : UserDefaults.standard.bool(forKey: "zb.enabled")
+        if enabled {
+            GammaController.apply(intensity: intensity == 0 ? 0.5 : intensity)
+        }
+    }
+
+    @objc private func togglePopover() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        }
+    }
+    func applicationWillTerminate(_ notification: Notification) {
+        GammaController.reset()
+    }
+}
